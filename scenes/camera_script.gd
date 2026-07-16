7ffdce868cbde707f6660bc12afd9f6a8ef3604b
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2026-07-16 11:04:39 +0100

    camera movement done

---
+++ scenes/camera_script.gd
@@ -0,0 +1,63 @@
+extends Camera2D
+
+var screen_size
+
+
+@export var player: CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player.velocity.y < 0 and player.global_position.y < global_position.y:
+		global_position.y = player.global_position.y
+
+	if player.global_position.y > global_position.y + screen_size.y / 2:
+		# var z_position = global_position.lerp(player.global_position, 5 * delta)
+		global_position.y = player.global_position.y + screen_size.y / 2
+		# global_position.y = z_position.y
+
+# func _process(delta):
+# 	var should_follow := false
+
+# 	if player.velocity.y < 0 and player.global_position.y < global_position.y:
+# 		should_follow = true
+
+# 	if player.global_position.y > global_position.y + screen_size.y / 2:
+# 		should_follow = true
+
+# 	if should_follow:
+# 		global_position.y = lerp(
+# 			global_position.y,
+# 			player.global_position.y,
+# 			5.0 * delta
+# 		)
+
+# How do I now create a scene for this, would the camera 2d be in the main or how?
+
+# if player.velocity.y < 0 and player.global_position.y < global_position.y:
+#     global_position.y = player.global_position.y
+# if player.global_position.y > global_position.y + screen_height / 2:
+#     global_position.y = player.global_position.y
+
+# global_position = global_position.lerp(player.global_position, 5 * delta)
+
+
+# func _process(delta):
+# 	var should_follow := false
+
+# 	if player.velocity.y < 0 and player.global_position.y < global_position.y:
+# 		should_follow = true
+
+# 	if player.global_position.y > global_position.y + screen_size.y / 2:
+# 		should_follow = true
+
+# 	if should_follow:
+# 		global_position.y = lerp(
+# 			global_position.y,
+# 			player.global_position.y,
+# 			5.0 * delta
+# 		)
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,57 @@
 extends Node
 
+@export var platform_scene: PackedScene
+@export var broken_platform_scene: PackedScene
+@export var platform_with_spring_scene: PackedScene
+
+var platform_container: Array[Node] = []
+var screen_size
+
+var y = 18
+var jump_value = 17
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport().size
+	spawn_platform()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawn_platform():
+	for a in range(49):
+		if a % jump_value == 0:
+			var platform = platform_scene.instantiate()
+			# Later change the multiplier from 3 to 4 in the y co-ordinate of the platform position.
+			platform.global_position = Vector2(randi_range(36, 324), (screen_size.y - 18) - 3 * a * 9)
+			platform_container.append(platform)
+		else:
+			var randomizer = randi_range(0, 3)
+			
+			if randomizer == 0:
+				# That is no platform is created.
+				continue
+			elif randomizer == 1:
+				var platform = platform_scene.instantiate()
+				# Later change the multiplier from 3 to 4 in the y co-ordinate of the platform position.
+				platform.global_position = Vector2(randi_range(36, 324), (screen_size.y - 18) - 4 * a * 9)
+				platform_container.append(platform)
+			elif randomizer == 2:
+				var broken_platform = broken_platform_scene.instantiate()
+				# Later change the multiplier from 3 to 4 in the y co-ordinate of the platform position.
+				broken_platform.global_position = Vector2(randi_range(36, 324), (screen_size.y - 18) - 4 * a * 9)
+				platform_container.append(broken_platform)
+			elif randomizer == 3:
+				var platform_with_spring = platform_with_spring_scene.instantiate()
+				# Later change the multiplier from 3 to 4 in the y co-ordinate of the platform position.
+				platform_with_spring.global_position = Vector2(randi_range(36, 324), (screen_size.y - 18) - 4 * a * 9)
+				platform_container.append(platform_with_spring)
+			
+	for platform in platform_container:
+		add_child(platform)
+	
+
+
